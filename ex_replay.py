c2a60db117df895d5439607ba2f6cdb3303267d6
Siddhant Sahu <siddhant_sahu@outlook.com>
2022-03-22 05:47:58 +0100

    added compute q values func

--- ex_replay.py
+++ ex_replay.py
@@ -9,6 +9,8 @@ from generate_game import *
 from Chess_env import *
 import random
 
+import argparse
+
 parser = argparse.ArgumentParser(description='Hyperparameter of SARSA algorithm')
 parser.add_argument('--epsilon', type=float, help='Starting value of Epsilon for the Epsilon-Greedy policy', default=0.2)
 parser.add_argument('--beta', type=float, help='The parameter sets how quickly the value of Epsilon is decaying',default=0.00005)
@@ -41,3 +43,24 @@ def EpsilonGreedy_Policy(Qvalues, epsilon, allowed_a):
         a = a[idx]
             
     return a
+
+def ComputeQvalues(W1, W2, bias_W1, bias_W2, X, hiddenactivfunction , outeractivfunction):
+    ## Qvalues=np.matmul(W2, np.matmul(W1,X)) ## this is direct computation of hidden layer and then output layer, without applying any non linear activation function
+    ## below is a better solution:
+    # Neural activation: input layer -> hidden layer
+    H1 = np.matmul(W1,X)+bias_W1 ## make sure that bias_W1 does not need to be transposed
+    # if hidden activ function is given:
+    if (hiddenactivfunction == 1):
+        H1 = np.round(1/(1+np.exp(-H1)), 5) ## sigmoid
+    elif(hiddenactivfunction == 2):
+         H1 = (H1>0).astype(int)*H1  ## RELU
+        
+    Qvalues = np.matmul(W2,H1) + bias_W2
+    
+    #if outer activ function is given
+    if (outeractivfunction == 1):
+        Qvalues = np.round(1/(1+np.exp(- Qvalues)), 5)
+    elif (outeractivfunction == 2):
+        Qvalues = (Qvalues>0).astype(int)*Qvalues
+
+    return Qvalues
