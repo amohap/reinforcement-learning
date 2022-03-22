ea137440d194e80d16b7f570b7dae3d15605617f
Siddhant Sahu <siddhant_sahu@outlook.com>
2022-03-22 05:53:47 +0100

    exreplay core logic implementation

--- ex_replay.py
+++ ex_replay.py
@@ -80,3 +80,100 @@ N_h=200                      ## NUMBER OF HIDDEN NODES
 ## INITALISE YOUR NEURAL NETWORK... Here weights from input to hidden layer and from the hidden layer to output layer are initialized
 W1 = np.random.randn(N_h, N_in) * np.sqrt(1 / (N_in)) 
 W2 = np.random.randn(N_a, N_h) * np.sqrt(1 / (N_h))
+
+bias_W1 = np.zeros((N_h,))
+bias_W2 = np.zeros((N_a,))
+
+# REWARD SCHEME DEFAULT
+# Reward if episode is not ended: 0
+# Reward if checkmate: 1
+# Reward if draw: 0
+
+N_episodes = 100000 # THE NUMBER OF GAMES TO BE PLAYED 100000
+
+hiddenactivfunction = 0
+outeractivfunction = 1
+
+# SAVING VARIABLES
+R_save = np.zeros([N_episodes, 1])
+N_moves_save = np.zeros([N_episodes, 1])
+Delta_save = np.zeros([N_episodes, 1])
+
+for n in range(N_episodes):
+    S,X,allowed_a=env.Initialise_game()
+    epsilon_f = epsilon_0 / (1 + beta * n)   ## DECAYING EPSILON
+    Done=0                                   ## SET DONE TO ZERO (BEGINNING OF THE EPISODE)
+    i = 1  ## COUNTER FOR NUMBER OF ACTIONS
+    d = {}
+    
+    
+    while (Done==0 and i<50):                           ## START THE EPISODE
+       
+        Qvalues = ComputeQvalues(W1, W2, bias_W1, bias_W2, X, hiddenactivfunction , outeractivfunction)
+        a=EpsilonGreedy_Policy(Qvalues,epsilon_f, allowed_a)
+        S_next, X_next,allowed_a_next,R,Done=env.OneStep(a)
+        dataToStore = (X,a,R,X_next,Done, allowed_a_next)
+        d[i] = dataToStore
+     
+        
+        if Done==1:
+
+            R_save[n,]=np.copy(R)
+            N_moves_save[n,]=i
+        
+            while i>0: 
+                x = d[i][0]
+                a = d[i][1]
+                r = d[i][2] 
+                next_x = d[i][3]
+                done = d[i][4]
+                allowed = d[i][5]
+                Qvalues=ComputeQvalues(W1, W2, bias_W1, bias_W2, x, hiddenactivfunction , outeractivfunction)
+            
+                if done:
+                    dEdQ=r-Qvalues[a]
+                else:
+                    Qvalues1=ComputeQvalues(W1, W2, bias_W1, bias_W2, next_x, hiddenactivfunction , outeractivfunction)
+                    a1=EpsilonGreedy_Policy(Qvalues1,0, allowed)
+                    dEdQ=R+gamma*Qvalues1[a1]- Qvalues[a]
+
+                Delta_save[n,]=0.5*(dEdQ)*(dEdQ)
+                
+                ## update W2 and B2
+                dQdY = 1  
+                if outeractivfunction == 1:
+                        dYdQ = Qvalues[a]*(1-Qvalues[a])
+                elif outeractivfunction == 2:
+                        dYdQ = (Qvalues[a]>0).astype(int)
+
+                H = np.matmul(W1,X) + bias_W1
+                if hiddenactivfunction == 1:
+                         H = np.round(1/(1+np.exp(-H)), 5)
+                elif(hiddenactivfunction == 2):
+                         H = (H>0).astype(int)*H
+                dYdW = H
+
+                W2[a,:]=W2[a,:]+eta*dEdQ*dQdY*dYdW
+                bias_W2[a]=bias_W2[a]+eta*dEdQ*dQdY
+
+                ## update W1 and B1 after W2 and B2 were updated
+                if hiddenactivfunction == 1:
+                    dYdZ =  (W2[a,:].reshape(1, 200) * H*(1-H).reshape(1, 200)).reshape(200,1)
+                elif(hiddenactivfunction == 2):
+                    dYdZ =  (W2[a,:].reshape(1, 200) * (H>0).astype(int)).reshape(200,1)
+                else:
+                    dYdZ =  W2[a,:].reshape(200, 1)
+
+                dZDW = X.reshape(1, 58)        
+                W1[:,:]=W1[:,:]+ eta*dEdQ*dQdY*dYdZ*dZDW 
+                bias_W1=bias_W1+ eta*dEdQ*dQdY*dYdZ.reshape(200,) 
+                i = i-1
+                break
+        
+        else:
+            
+            S=np.copy(S_next)
+            X=np.copy(X_next)
+            allowed_a = np.copy(allowed_a_next)
+            i = i+1
+
