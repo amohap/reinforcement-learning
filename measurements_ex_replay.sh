a923781d15ebf0136343d0a71b385b025b2e6bc7
Siddhant Sahu <siddhant_sahu@outlook.com>
2022-03-25 14:58:06 +0100

    hyperparameter update

--- measurements_ex_replay.sh
+++ measurements_ex_replay.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-epsilons=(1 2 4 8 16 32 64 128)
-betas=(-1 -2 -4 -8 -16 -32 -64 -128)
-gammas=(1 2 4 8 16 32 64 128)
-etas=(1 2 4 8 16 32 64 128)
+epsilons=(0.1 0.2 0.9)
+betas=(0.00005 0.0005 0.005)
+gammas=(0.8 0.85 1)
+etas=(0.0035 0.005 0.05)
 
 if [ ! -d experiments/ex_replay ]; then
   mkdir -p experiments/ex_replay;
